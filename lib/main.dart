1bdda8f5139a8695f09a85b6799ac7fde629da44
ritesh6271 <riteshyadav839@gmail.com>
2020-11-09 12:27:18 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_storage_video/FirebaseStorage/BottomNavigationBar.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: NavigationBar(),
+    );
+  }
+}
